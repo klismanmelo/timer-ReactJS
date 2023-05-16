785c5666e0bc085b9d09b50eceba0f158a9ec2be
klismanmelo <61260271+klismanmelo@users.noreply.github.com>
2023-05-16 10:21:37 -0300

    Criado e estilizado a Page History

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -40,6 +40,7 @@ export const HistoryList = styled.div`
       border-top: 4px solid ${(props) => props.theme['gray-800']};
       padding: 1rem;
       font-size: 0.875rem;
+      color: ${(props) => props.theme['gray-100']};
       line-height: 1.6;
       &:first-child {
         width: 50%;
@@ -49,5 +50,27 @@ export const HistoryList = styled.div`
         padding-right: 1.5rem;
       }
     }
+  }
+`
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 9999px;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
   }
 `
